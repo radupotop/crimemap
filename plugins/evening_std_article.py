1d318b621e99ff0143ab47bb3a5fdbb13ecdeff4
Radu Potop <radu@wooptoo.com>
2017-05-02 22:48:26 +0300

    Scrape aside and subtitle from article

--- plugins/evening_std_article.py
+++ plugins/evening_std_article.py
@@ -8,20 +8,29 @@ class EveningStdArticle():
     The Evening Standard Article scraper.
     """
 
+    @staticmethod
+    def __join_par(element):
+        return "\n".join([p.text for p in element.find_all('p')])
+
     @classmethod
     def scrape(cls, url):
         resp = requests.get(url)
         page = BeautifulSoup(resp.content, 'lxml')
         article = page.article
 
+        _intro = article.header.find(class_='intro')
+        _infobox = article.find(class_='ines_infobox')
+
         _scraped = {
             'title': article.header.h1.text.strip(),
+            'subtitle': _intro.text.strip() if _intro else None,
             'author': article.header.find(class_='author').text.strip(),
             'published_datetime': datetime.fromtimestamp(int(int(article.header.time.get('unixtime'))/1000)),
             'scrape_datetime': datetime.utcnow(),
             'media': json.dumps([img.get('src') for img in article.find_all('img') if img.get('title')]),
             'href': url,
-            'content': "\n".join([art.text for art in article.find_all('p')])
+            'content': cls.__join_par(article),
+            'aside': cls.__join_par(_infobox) if _infobox else None,
         }
 
         return _scraped
