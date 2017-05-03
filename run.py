132ba62c760df6a5256ac9b4b977b683f2e84439
Radu Potop <radu@wooptoo.com>
2017-05-03 11:34:39 +0300

    Task runner with asyncio

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import IntegrityError, InvalidRequestError
 from pprint import pprint
 import logging
-import time
+import asyncio
 
 logging.basicConfig(level=logging.INFO)
 log=logging.getLogger('Scraper')
@@ -20,22 +20,32 @@ Base.metadata.bind = db
 
 DBSession = sessionmaker(bind=db)
 
-while True:
+async def run_article_index():
+    """
+    Article index task
+    """
+    while True:
+        session = DBSession()
+        articles = EveningStd.scrape()
+        log.info('Scraping')
 
-    session = DBSession()
-    articles = EveningStd.scrape()
-    log.info('Scraping')
+        for art in articles:
+            try:
+                art_mdl = ArticleIndex(**art)
+                session.add(art_mdl)
+                session.commit()
+                log.info('Created entry: {}'.format(art['hash']))
+            except (IntegrityError, InvalidRequestError):
+                # entry exists
+                pass
 
-    for art in articles:
-        try:
-            art_mdl = ArticleIndex(**art)
-            session.add(art_mdl)
-            session.commit()
-            log.info('Created entry: {}'.format(art['hash']))
-        except (IntegrityError, InvalidRequestError):
-            # entry exists
-            pass
+        session.close()
+        pprint('----')
+        await asyncio.sleep(3600)
 
-    session.close()
-    pprint('----')
-    time.sleep(3600)
+
+loop = asyncio.get_event_loop()
+asyncio.ensure_future(run_article_index(), loop=loop)
+
+loop.run_forever()
+loop.close()
