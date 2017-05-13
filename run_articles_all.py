b829386853bf62bc98c0bf6b45788a20f96e99c6
Radu Potop <radu@wooptoo.com>
2017-05-14 00:35:49 +0100

    Scrape all articles

---
+++ run_articles_all.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from model import Base, ArticleIndex, ArticleContent
+from plugins import EveningStd, EveningStdArticle
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError, InvalidRequestError
+from pprint import pprint
+import logging
+
+logging.basicConfig(level=logging.INFO)
+log=logging.getLogger('Scraper')
+log.setLevel(logging.DEBUG)
+
+db = create_engine('postgresql://postgres@localhost/crimemap')
+Base.metadata.bind = db
+
+DBSession = sessionmaker(bind=db)
+
+def run_all_article_contents():
+    log.info('Scraping')
+    session = DBSession()
+
+    all_idx = session.query(ArticleIndex).all()
+
+    for art in all_idx:
+        scraped_article = EveningStdArticle.scrape(art.href)
+        model = ArticleContent(**scraped_article)
+        model.index_hash = art.hash
+        pprint(model)
+        session.add(model)
+        session.commit()
+
+    session.close()
+
+
+run_all_article_contents()
