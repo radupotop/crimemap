32efabc6e14da24f8b653c93830fa9afbb2733b2
Radu Potop <radu@wooptoo.com>
2017-08-06 21:23:03 +0100

    es article: get pub_time from meta

--- plugins/evening_std_article.py
+++ plugins/evening_std_article.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
+from dateutil import parser
+import re
 from .base_scraper import BaseScraper
 
 
@@ -24,15 +26,15 @@ class EveningStdArticle(BaseScraper):
             return
 
         _intro = article.find(class_='intro')
-        _published = article.time.get('unixtime') if hasattr(article, 'time') else None
         _author = article.find(class_='author')
         _infobox = article.find(class_='ines_infobox')
+        _pub_time = page.find('meta', property=re.compile(r'article:pub.*time'))
 
         _scraped = {
             'title': article.h1.text.strip() if hasattr(article, 'h1') else None,
             'subtitle': _intro.text.strip() if _intro else None,
             'author': _author.text.strip() if _author else None,
-            'published_datetime': datetime.fromtimestamp(int(int(_published)/1000)) if _published else None,
+            'published_datetime': parser.parse(_pub_time.get('content')) if hasattr(_pub_time, 'content') else None,
             'scrape_datetime': datetime.utcnow(),
             'media': [img.get('src') for img in article.find_all('img') if img.get('title')],
             'meta': {'tags': [tag.text for tag in article.find_all(attrs={'itemprop': 'keywords'})]},
